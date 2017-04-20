9a606fbb50aac146b7f7375fda7ef1894154f7c4
Xi Gao <xiga6451@colorado.edu>
2017-04-20 13:04:35 -0600

    Update NewUserLogin.php
    Updated to match file in .io directory.

--- NewUserLogin.php
+++ NewUserLogin.php
@@ -1,4 +1,5 @@
 <html>
+<link href='//fonts.googleapis.com/css?family=Overlock SC' rel='stylesheet'>
 <head>
 <title>New User Sign Up</title>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
@@ -7,60 +8,77 @@
  var clickBtnName = $(this).attr('name');
  var data = {'id': clickBtnName};
  $.post(ajaxurl, data, function(response) {
- window.location.href="http://localhost/NewUserLogin.php";
+ window.location.href="https://hound-dogs.herokuapp.com/NewUserLogin.php";
 });
 });
 });
 </script>
 <style>
-body {
-	color: blue;
+input[type=text], select {
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+}
+input[type=password], select {
+	width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+}
+input[type=submit] {
+    width: 50%;
+    background-color: #53c687;
+    color: white;
+    padding: 14px 20px;
+    margin: 20px 110px;
+    border: none;
+    border-radius: 4px;
+    font-family: 'Overlock SC';
+    font-size: 20px;
 }
 h1{
-	padding: 70px 0;
-	text-align: center;
+    padding: 40px;
+    text-align: center;
+    font-family: 'Overlock SC';
+    font-size: 50px;
+    color: green;
 }
-form {
-	
-	width:100%;
-	height: 200px;
+div {
+    width: 50%;
+    margin: auto;
+    border-radius: 5px;
+    background-color: none;
+    font-family: 'Overlock SC';
+    color: #006622;
 }
-table{
-	margin:0 auto;
-	z-index: 1;
-	width:0%;
-	height:50px;
+body {
+	background-image: url("https://www.ecotourismgc.com/wp-content/uploads/2015/09/background.jpg");
 }
 </style>
 </head>
 <body>
  <h1>Welcome New User!</h1>
  
- <form style = "text-align: center" enctype="multipart/form-data" action = "http://localhost/InsertUsers.php" method = "post">
- <table width = "200" border = "0">
- <tr>
- 	<td> Enter a username:
- 	</td>
- 	<td><input type = "text" name = "username" size = "30">
- 	</td>
- </tr>
- <tr>
- 	<td> Enter a password:
- 	</td>
- 	<td><input type = "text" name = "password" size = "30"> 
- 	</td>
- </tr>
- <tr>
- 	<td> Repeat password:
- 	</td>
- 	<td><input type = "text" name = "reppassword" size = "30"> 
- 	</td>
- </tr>
- <tr>
- 	<td><input type ="submit" value = "Enter">
- 	</td>
- </tr>
- </table>
- </form>
+ <div>
+  <form style = "text-align: center" enctype="multipart/form-data" action = "http://localhost/InsertUsers.php" method = "post">
+    <label>Username</label>
+    <input type="text" name="username" placeholder="Username" size="30">
+
+    <label>Password</label>
+    <input type="password" name="password" placeholder="Password" size="30">
+  	
+    <label>Repeat Password</label>
+    <input type="password" name="reppassword" placeholder="Password" size="30">
+    
+    <input type="submit" value="Submit">
+  </form>
+</div>
  </body>
  </html>
